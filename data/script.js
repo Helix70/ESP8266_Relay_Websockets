ef93242d8806b88c276ad64cd95b46eb67113004
Helix70 <lexusjosh@gmail.com>
2022-12-14 20:26:35 +1000

    Update for 8 relays (WIP)

--- data/script.js
+++ data/script.js
@@ -28,10 +28,28 @@ function onMessage(event) {
 function updateRelays(event) {
   const relay1state = msg.RELAY1 ? "ON" : "OFF";
   const relay2state = msg.RELAY2 ? "ON" : "OFF";
+  const relay3state = msg.RELAY3 ? "ON" : "OFF";
+  const relay4state = msg.RELAY4 ? "ON" : "OFF";
+  const relay5state = msg.RELAY5 ? "ON" : "OFF";
+  const relay6state = msg.RELAY6 ? "ON" : "OFF";
+  const relay7state = msg.RELAY7 ? "ON" : "OFF";
+  const relay8state = msg.RELAY8 ? "ON" : "OFF";
+
   document.getElementById('relay1state').innerHTML = relay1state;
   document.getElementById('relay2state').innerHTML = relay2state;
+  document.getElementById('relay3state').innerHTML = relay3state;
+  document.getElementById('relay4state').innerHTML = relay4state;
+  document.getElementById('relay5state').innerHTML = relay5state;
+  document.getElementById('relay6state').innerHTML = relay6state;
+  document.getElementById('relay7state').innerHTML = relay7state;
+  document.getElementById('relay8state').innerHTML = relay8state;
+  
   document.getElementById('relay1toggle').disabled = msg.RELAY1 || msg.RELAY2;
   document.getElementById('relay2toggle').disabled = msg.RELAY1 || msg.RELAY2;
+  document.getElementById('relay5toggle').disabled = msg.RELAY5 || msg.RELAY6;
+  document.getElementById('relay6toggle').disabled = msg.RELAY5 || msg.RELAY6;
+  document.getElementById('relay7toggle').disabled = msg.RELAY7 || msg.RELAY8;
+  document.getElementById('relay8toggle').disabled = msg.RELAY7 || msg.RELAY8;
 }
 
 function onLoad(event) {
@@ -42,12 +60,24 @@ function initButtons() {
   document.getElementById('toggle').addEventListener('click', toggle);
   document.getElementById('relay1toggle').addEventListener('click', toggle);
   document.getElementById('relay2toggle').addEventListener('click', toggle);
+  document.getElementById('relay3toggle').addEventListener('click', toggle);
+  document.getElementById('relay4toggle').addEventListener('click', toggle);
+  document.getElementById('relay5toggle').addEventListener('click', toggle);
+  document.getElementById('relay6toggle').addEventListener('click', toggle);
+  document.getElementById('relay7toggle').addEventListener('click', toggle);
+  document.getElementById('relay8toggle').addEventListener('click', toggle);
 }
 
 function toggle (callee) {
   updateRelays({
     RELAY1: callee.srcElement.id == 'relay1toggle',
     RELAY2: callee.srcElement.id == 'relay2toggle',
+    RELAY3: callee.srcElement.id == 'relay3toggle',
+    RELAY4: callee.srcElement.id == 'relay4toggle',
+    RELAY5: callee.srcElement.id == 'relay5toggle',
+    RELAY6: callee.srcElement.id == 'relay6toggle',
+    RELAY7: callee.srcElement.id == 'relay7toggle',
+    RELAY8: callee.srcElement.id == 'relay8toggle',
   });
   console.log("sending message: ", callee.srcElement.id);
   websocket.send(callee.srcElement.id);
--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@
 #include "credentials.h"
 
 // Replace with your network credentials
-//const char* ssid = "REPLACE_WITH_YOUR_SSID";
-//const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+// const char* ssid = "REPLACE_WITH_YOUR_SSID";
+// const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 uint32_t elapsed = 0;
 uint32_t timer = 0;
@@ -33,171 +33,302 @@ AsyncWebSocket ws("/ws");
 // Definition of the LED component
 // ----------------------------------------------------------------------------
 
-struct OutputPin {
-    // state variables
-    const uint8_t pin;
-    uint8_t  on;
-    const uint8_t on_state;   // LOW active or HIGH active
+struct OutputPin
+{
+  // state variables
+  const uint8_t pin;
+  uint8_t on;
+  const uint8_t on_state; // LOW active or HIGH active
+  const char *label;
 
-    // methods
-    void update() {
-        digitalWrite(pin, on ? HIGH : LOW);
-    }
+  // methods
+  void update()
+  {
+    digitalWrite(pin, on ? HIGH : LOW);
+  }
 
-    void low() {
-      on = !on_state;
-    }
+  void low()
+  {
+    on = !on_state;
+  }
 
-    void high() {
-      on = on_state;
-    }
+  void high()
+  {
+    on = on_state;
+  }
 
-    void toggle() {
-      on = !on;
-    }
+  void toggle()
+  {
+    on = !on;
+  }
 };
 
 // ----------------------------------------------------------------------------
 // LittleFS initialization
 // ----------------------------------------------------------------------------
 
-OutputPin onboard_led = { LED_BUILTIN, false, HIGH };
+OutputPin onboard_led = {LED_BUILTIN, false, HIGH, "LED"};
+
+#define NUM_RELAYS 8
 
 OutputPin relays[] = {
-  { 5, false, HIGH },
-  { 4, false, HIGH },
-  { 0, false, HIGH },
-  { 15, false, HIGH },
-  { 13, false, HIGH },
-  { 12, false, HIGH },
-  { 14, false, HIGH },
-  { 16, false, HIGH }
-};
+    {5, false, HIGH, "RELAY1"},
+    {4, false, HIGH, "RELAY2"},
+    {0, false, HIGH, "RELAY3"},
+    {15, false, HIGH, "RELAY4"},
+    {13, false, HIGH, "RELAY5"},
+    {12, false, HIGH, "RELAY6"},
+    {14, false, HIGH, "RELAY7"},
+    {16, false, HIGH, "RELAY8"}};
 
-void initLittleFS() {
-  if (!LittleFS.begin()) {
+void initLittleFS()
+{
+  if (!LittleFS.begin())
+  {
     Serial.println("Cannot mount LittleFS volume...");
-    while (1) {
-        onboard_led.on = millis() % 200 < 50;
-        onboard_led.update();
+    while (1)
+    {
+      onboard_led.on = millis() % 200 < 50;
+      onboard_led.update();
     }
   }
   Serial.println("LittleFS volume mounted...");
 }
 
-void notifyClients() {
+void notifyClients()
+{
   Serial.println("notifying clients");
 
-  char buffer[50];
-  sprintf(
-    buffer, 
-    "{\"LED\":%s,\"RELAY1\":%s,\"RELAY2\":%s}",
-    onboard_led.on ? "true" : "false",
-    relays[0].on ? "true" : "false",
-    relays[1].on ? "true" : "false"
-    );
+  char buffer[(((NUM_RELAYS + 1) * 15) + 2)];
+  char temp[20];
+
+  sprintf(buffer, "{\"%s\":%s", onboard_led.label, onboard_led.on ? "true" : "false");
+  for (int i = 0; i < NUM_RELAYS; i++)
+  {
+    sprintf(temp, ",\"%s\":%s", relays[i].label, relays[i].on ? "true" : "false");
+    strcat(buffer, temp);
+  }
+  strcat(buffer, "}");
   ws.textAll(buffer);
 }
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  char buffer[len+1];   
-  snprintf(buffer, len+1, "%s", data);  // convert char array to null terminated
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  char buffer[len + 1];
+  Serial.printf("len=%d\n",len);
+  snprintf(buffer, len + 1, "%s", data); // convert char array to null terminated
   Serial.printf("handle web socket message: %s\n", buffer);
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
     bool notify = false;
-    if (strcmp((char*)buffer, "toggle") == 0) {
+    if (strcmp((char *)buffer, "toggle") == 0)
+    {
       onboard_led.on = !onboard_led.on;
       onboard_led.update();
       notify = true;
     }
 
-    if (relays[0].on || relays[1].on) {
+    if (relays[0].on || relays[1].on)
+    {
       // skip updates
     }
-    else if (strcmp((char*)buffer, "relay1toggle") == 0) {
+    else if (strcmp((char *)buffer, "relay1toggle") == 0)
+    {
       relays[0].toggle();
       relays[0].update();
       notify = true;
       countdown = millis();
     }
-    else if (strcmp((char*)buffer, "relay2toggle") == 0) {
+    else if (strcmp((char *)buffer, "relay2toggle") == 0)
+    {
       relays[1].toggle();
       relays[1].update();
       notify = true;
       countdown = millis();
     }
 
-    if (notify) {
+    if (strcmp((char *)buffer, "relay3toggle") == 0)
+    {
+      relays[2].toggle();
+      relays[2].update();
+      notify = true;
+      countdown = millis();
+    }
+    else if (strcmp((char *)buffer, "relay4toggle") == 0)
+    {
+      relays[3].toggle();
+      relays[3].update();
+      notify = true;
+      countdown = millis();
+    }
+
+
+    if (relays[4].on || relays[5].on)
+    {
+      // skip updates
+    }
+    else if (strcmp((char *)buffer, "relay5toggle") == 0)
+    {
+      relays[4].toggle();
+      relays[4].update();
+      notify = true;
+      countdown = millis();
+    }
+    else if (strcmp((char *)buffer, "relay6toggle") == 0)
+    {
+      relays[5].toggle();
+      relays[5].update();
+      notify = true;
+      countdown = millis();
+    }
+
+    if (relays[6].on || relays[7].on)
+    {
+      // skip updates
+    }
+    else if (strcmp((char *)buffer, "relay7toggle") == 0)
+    {
+      relays[6].toggle();
+      relays[6].update();
+      notify = true;
+      countdown = millis();
+    }
+    else if (strcmp((char *)buffer, "relay8toggle") == 0)
+    {
+      relays[7].toggle();
+      relays[7].update();
+      notify = true;
+      countdown = millis();
+    }
+
+    if (notify)
+    {
       notifyClients();
     }
   }
 }
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-    Serial.println("on event");
-    switch (type) {
-      case WS_EVT_CONNECT:
-        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-        break;
-      case WS_EVT_DISCONNECT:
-        Serial.printf("WebSocket client #%u disconnected\n", client->id());
-        break;
-      case WS_EVT_DATA:
-        handleWebSocketMessage(arg, data, len);
-        break;
-      case WS_EVT_PONG:
-      case WS_EVT_ERROR:
-        break;
+             void *arg, uint8_t *data, size_t len)
+{
+  Serial.println("on event");
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
   }
 }
 
-void initWebSocket() {
+void initWebSocket()
+{
   Serial.println("init web socket");
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
-void initWiFi() {
+void initWiFi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
-  while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(500);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
   }
   Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
 }
 
-String processor(const String& var){
+String processor(const String &var)
+{
   Serial.println(var);
 
-  if(var == "STATE"){
-    if (onboard_led.on){
+  if (var == "STATE")
+  {
+    if (onboard_led.on)
+    {
       return "ON";
     }
-    else{
+    else
+    {
       return "OFF";
     }
   }
-  if (var == "RELAY1STATE") {
+  if (var == "RELAY1STATE")
+  {
     return relays[0].on ? "ON" : "OFF";
   }
-  if (var == "RELAY2STATE") {
+  if (var == "RELAY2STATE")
+  {
     return relays[1].on ? "ON" : "OFF";
   }
-  if (var == "RELAY1DISABLED") {
+  if (var == "RELAY3STATE")
+  {
+    return relays[2].on ? "ON" : "OFF";
+  }
+  if (var == "RELAY4STATE")
+  {
+    return relays[3].on ? "ON" : "OFF";
+  }
+  if (var == "RELAY5STATE")
+  {
+    return relays[4].on ? "ON" : "OFF";
+  }
+  if (var == "RELAY6STATE")
+  {
+    return relays[5].on ? "ON" : "OFF";
+  }
+  if (var == "RELAY7STATE")
+  {
+    return relays[6].on ? "ON" : "OFF";
+  }
+  if (var == "RELAY8STATE")
+  {
+    return relays[7].on ? "ON" : "OFF";
+  }
+
+  if (var == "RELAY1DISABLED")
+  {
     return relays[1].on ? "disabled" : "";
   }
-  if (var == "RELAY2DISABLED") {
+  if (var == "RELAY2DISABLED")
+  {
     return relays[0].on ? "disabled" : "";
   }
 
+  if (var == "RELAY5DISABLED")
+  {
+    return relays[5].on ? "disabled" : "";
+  }
+  if (var == "RELAY6DISABLED")
+  {
+    return relays[4].on ? "disabled" : "";
+  }
+
+  if (var == "RELAY7DISABLED")
+  {
+    return relays[7].on ? "disabled" : "";
+  }
+  if (var == "RELAY8DISABLED")
+  {
+    return relays[6].on ? "disabled" : "";
+  }
   return String();
 }
 
-void setup(){
+void setup()
+{
   uint8_t i;
 
   // Serial port for debugging purposes
@@ -207,23 +338,26 @@ void setup(){
   onboard_led.on = !onboard_led.on_state;
   onboard_led.update();
 
-  for (i=0; i<8; i++) {
+  // initialise relays
+  for (i = 0; i < NUM_RELAYS; i++)
+  {
     pinMode(relays[i].pin, OUTPUT);
     relays[i].low();
     relays[i].update();
   }
 
+  // Mount Filesystem
   initLittleFS();
   // Connect to Wi-Fi
   initWiFi();
 
   initWebSocket();
 
-// Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("on /");
-    request->send(LittleFS, "/index.html", "text/html",false,processor);
-  });
+    request->send(LittleFS, "/index.html", "text/html",false,processor); });
   server.serveStatic("/", LittleFS, "/");
 
   // Start server
@@ -232,17 +366,20 @@ void setup(){
   notifyClients();
 }
 
-void loop() {
+void loop()
+{
   uint8_t i;
 
   elapsed = millis();
-  if ((elapsed - timer) > 5000) {
+  if ((elapsed - timer) > 5000)
+  {
     onboard_led.on = !onboard_led.on;
     timer = elapsed;
     notifyClients();
   }
 
-  if ((elapsed - countdown) > COUNTDOWN_TIMEOUT_MS && countdown != 0) {
+  if ((elapsed - countdown) > COUNTDOWN_TIMEOUT_MS && countdown != 0)
+  {
     relays[0].low();
     relays[1].low();
     countdown = 0;
@@ -251,9 +388,9 @@ void loop() {
 
   ws.cleanupClients();
 
-  for (i=0; i<8; i++) {
+  for (i = 0; i < 8; i++)
+  {
     relays[i].update();
   }
   onboard_led.update();
 }
-
