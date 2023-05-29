d47b48791570f95177ee6503460241b142dbeae1
Josh Adler <lexusjosh@gmail.com>
2023-05-30 06:44:55 +1000

    Format code

--- src/main.cpp
+++ src/main.cpp
@@ -525,16 +525,21 @@ void setup()
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
-    } else { // U_FS
+    } 
+    else 
+    { // U_FS
       type = "filesystem";
     }
         // NOTE: if updating FS this would be the place to unmount FS using FS.end()
     LittleFS.end();
     Serial.println("Start updating " + type); });
+
   ArduinoOTA.onEnd([]()
                    { Serial.println("\nEnd"); });
+
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
                         { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+
   ArduinoOTA.onError([](ota_error_t error)
                      {
     Serial.printf("Error[%u]: ", error);
@@ -543,6 +548,7 @@ void setup()
     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
     else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+
   ArduinoOTA.begin();
 }
 
