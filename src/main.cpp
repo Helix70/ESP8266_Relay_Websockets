7c21e6b0ecd1fd66f0a8f46cc33ca19f27403365
Josh Adler <lexusjosh@gmail.com>
2023-05-30 06:42:46 +1000

    Use correct libs, add OTA progress code, add partition.csv

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,16 @@
 #include <ArduinoOTA.h>
 #include "LittleFS.h"
 
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
+#endif
+
 #include <ESPAsyncWebServer.h>
+//#include <AsyncElegantOTA.h>
 
 #include "credentials.h"
 
@@ -505,12 +512,39 @@ void setup()
 
   // Start server
   Serial.println("starting server");
+
+  //AsyncElegantOTA.begin(&server); 
   server.begin();
   // notifyClients();
 
   /**
    * Enable OTA update
    */
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+        // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    LittleFS.end();
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
   ArduinoOTA.begin();
 }
 
