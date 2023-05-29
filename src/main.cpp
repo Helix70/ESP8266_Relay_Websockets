0e3cfcf904340f08f2e2ba208cf630ff478eda5d
Josh Adler <lexusjosh@gmail.com>
2023-05-30 06:43:32 +1000

    Format code

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@
 #include "LittleFS.h"
 
 #if defined(ESP8266)
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
 #elif defined(ESP32)
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
 #endif
 
 #include <ESPAsyncWebServer.h>
-//#include <AsyncElegantOTA.h>
+// #include <AsyncElegantOTA.h>
 
 #include "credentials.h"
 
@@ -513,14 +513,15 @@ void setup()
   // Start server
   Serial.println("starting server");
 
-  //AsyncElegantOTA.begin(&server); 
+  // AsyncElegantOTA.begin(&server);
   server.begin();
   // notifyClients();
 
   /**
    * Enable OTA update
    */
-  ArduinoOTA.onStart([]() {
+  ArduinoOTA.onStart([]()
+                     {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -529,22 +530,19 @@ void setup()
     }
         // NOTE: if updating FS this would be the place to unmount FS using FS.end()
     LittleFS.end();
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.println("Start updating " + type); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
+    else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
   ArduinoOTA.begin();
 }
 
